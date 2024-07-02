3a9ab95102a2efc5622083a6e97c952edf206199
venkatprasad0102 <120673949+venkatprasad0102@users.noreply.github.com>
2024-07-02 12:49:42 +0530

    Update my_first_program.py

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,13 +1,20 @@
 from nada_dsl import *
 
 
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
 def nada_main():
     party1 = Party(name="Party1")
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    # Compute the product of my_int1 and my_int2
-    result = my_int1 * my_int2
+    # Compute the greatest common divisor of my_int1 and my_int2
+    gcd_result = gcd(my_int1, my_int2)
 
     # Return the result as the output
-    return [Output(result, "product_output", party1)]
+    return [Output(gcd_result, "gcd_output", party1)]
+
